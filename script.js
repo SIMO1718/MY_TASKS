161039b7ac52e43100f1ed03fdc7820281dfa5e6
SIMO1718 <waterfire.0.0.0.0.0.0@gmail.com>
2026-01-01 20:02:43 +0100

    v1.0

---
+++ script.js
@@ -0,0 +1,32 @@
+// script.js
+function addTask() {
+    const input = document.getElementById('taskInput');
+    const ul = document.getElementById('taskList');
+
+    if (input.value === '') {
+        alert("كتب شي حاجة أولا أ صاحبي!");
+        return;
+    }
+
+    // 1. إنشاء عنصر li جديد
+    const li = document.createElement('li');
+    li.innerHTML = input.value;
+
+    // 2. فاش نكليكيو على المهمة، تولي "Checked"
+    li.onclick = function() {
+        this.classList.toggle('checked');
+    };
+
+    // 3. إضافة زر الحذف (X)
+    const span = document.createElement('span');
+    span.innerHTML = "\u00D7"; // علامة X
+    span.onclick = function() {
+        this.parentElement.remove(); // حذف المهمة
+    };
+
+    li.appendChild(span);
+    ul.appendChild(li);
+
+    // 4. خوي الـ input باش تكتب مهمة خرى
+    input.value = '';
+}
